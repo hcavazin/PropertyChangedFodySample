617305890d9a4e8850618a4ff122d024fa3f16f5
Hudson Cavazin <hcavazin@hotmail.com>
2018-04-04 11:11:32 -0300

    test

---
+++ Sample/Sample/Class1.cs
@@ -0,0 +1,13 @@
+﻿using Caliburn.Micro;
+
+namespace Sample
+{
+    public class Class1 : Screen
+    {
+        #region Properties
+
+        public string Test { get; set; }
+
+        #endregion
+    }
+}
